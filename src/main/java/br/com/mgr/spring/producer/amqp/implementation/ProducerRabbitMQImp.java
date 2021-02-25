722d7c0ca9cf9f1d1ad0841a957f224c4aae4124
Mauri <maurigre@gmail.com>
2021-02-25 08:36:40 -0300

    Criando a implementacao do Producer

--- src/main/java/br/com/mgr/spring/producer/amqp/implementation/ProducerRabbitMQImp.java
+++ src/main/java/br/com/mgr/spring/producer/amqp/implementation/ProducerRabbitMQImp.java
@@ -1,15 +1,33 @@
 package br.com.mgr.spring.producer.amqp.implementation;
 
 import br.com.mgr.spring.producer.amqp.AmqpProducer;
+import br.com.mgr.spring.producer.configuration.ProducerRabbitConfiguration;
 import br.com.mgr.spring.producer.dto.MessageDto;
+import org.springframework.amqp.AmqpRejectAndDontRequeueException;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ProducerRabbitMQImp implements AmqpProducer<MessageDto> {
 
+    @Value("${spring.rabbitmq.request.routing-key.procuder}")
+    private String queue;
+
+    @Value("${spring.rabbitmq.request.exchange.procuder}")
+    private String exchange;
+
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
 
     @Override
-    public void producer(MessageDto messageDto) {
+    public void producer(MessageDto message) {
+        try {
+            rabbitTemplate.convertAndSend(exchange, queue, message);
 
+        } catch (Exception ex){
+            throw new AmqpRejectAndDontRequeueException(ex);
+        }
     }
 }
